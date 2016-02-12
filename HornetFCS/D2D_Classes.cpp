592c28fb4866ef4ecfdd8f10edb74417d6e7066f
Orion <ollyau@domain.tld>
2016-02-11 22:08:57 -0800

    Remove unused code and fixes for resource management.

--- HornetFCS/D2D_Classes.cpp
+++ HornetFCS/D2D_Classes.cpp
@@ -12,151 +12,19 @@ namespace Gauge
 
 //-----------------------------------------------------------------------------
 
-D2DGeo2::D2DGeo2(ID2D1Factory*& srcfactory, bool open)
-    :
-    m_geo(nullptr),
-    m_sink(nullptr),
-    m_transformedGeo(nullptr)
-{
-    srcfactory->CreatePathGeometry(&m_geo);
-    if (open)
-    {
-        m_geo->Open(&m_sink);
-    }
-}
-
-D2DGeo2::~D2DGeo2()
-{
-    if (m_transformedGeo)
-    {
-        m_transformedGeo->Release();
-    }
-    if (m_geo)
-    {
-        m_geo->Release();
-    }
-    if (m_sink)
-    {
-        m_sink->Release();
-    }
-}
-
-void D2DGeo2::open_sink()
-{
-    m_geo->Open(&m_sink);
-}
-
-void D2DGeo2::close_sink()
-{
-    if (m_sink)
-    {
-        m_sink->Close();
-        m_sink->Release();
-        m_sink = nullptr;
-    }
-}
-
-void D2DGeo2::transform(ID2D1Factory*& srcfactory, const D2D1_MATRIX_3X2_F mat)
-{
-    if (m_transformedGeo)
-    {
-        m_transformedGeo->Release();
-        m_transformedGeo = nullptr;
-    }
-    transform(srcfactory, m_transformedGeo, mat);
-}
-
-void D2DGeo2::transform(ID2D1Factory*& srcfactory, ID2D1TransformedGeometry*& dest_t_geo, const D2D1_MATRIX_3X2_F mat)
-{
-    srcfactory->CreateTransformedGeometry(m_geo, mat, &dest_t_geo);
-}
-
-//-----------------------------------------------------------------------------
-
-ID2D1PathGeometry* combine_multiple_transformed_geometries(ID2D1Factory*& srcFactory, int geo_count, ID2D1TransformedGeometry* geos[])
-{
-    ID2D1PathGeometry* path_geo_1 = nullptr;
-    ID2D1PathGeometry* path_geo_2 = nullptr;
-
-    srcFactory->CreatePathGeometry(&path_geo_1);
-    srcFactory->CreatePathGeometry(&path_geo_2);
-
-    for (int i = 0; i < geo_count; i++)
-    {
-        ID2D1GeometrySink* cmpl_s1 = nullptr;
-        ID2D1GeometrySink* cmpl_s2 = nullptr;
-
-        if (i % 2 == 0)
-        {
-            //copying into 1
-            path_geo_1->Open(&cmpl_s1);
-
-            if (i == 0)
-            {
-                geos[i]->CombineWithGeometry(geos[i], D2D1_COMBINE_MODE_UNION, nullptr, cmpl_s1);
-            }
-            else
-            {
-                geos[i]->CombineWithGeometry(path_geo_2, D2D1_COMBINE_MODE_UNION, nullptr, 0.0f, cmpl_s1);
-            }
-
-            cmpl_s1->Close();
-            cmpl_s1->Release();
-            if (i != 0)
-            {
-                path_geo_2->Release();
-                srcFactory->CreatePathGeometry(&path_geo_2);
-            }
-            //cmpl_g1 now contains the geometry so far
-        }
-        else
-        {
-            //copying into 2
-            path_geo_2->Open(&cmpl_s2);
-
-            geos[i]->CombineWithGeometry(path_geo_1, D2D1_COMBINE_MODE_UNION, nullptr, cmpl_s2);
-
-            cmpl_s2->Close();
-            cmpl_s2->Release();
-            path_geo_1->Release();
-            srcFactory->CreatePathGeometry(&path_geo_1);
-            //cmpl_g2 now contains the geometry so far
-        }
-    }
-
-    if (geo_count % 2 == 0)
-    {
-        if (path_geo_1)
-        {
-            path_geo_1->Release();
-        }
-        return path_geo_2;
-    }
-    else
-    {
-        if (path_geo_2)
-        {
-            path_geo_2->Release();
-        }
-        return path_geo_1;
-    }
-}
-
-//-----------------------------------------------------------------------------
-
 D2DGauge::D2DGauge(FCS::FBW::Ptr const & fbw)
     :
     m_draw(false),
     m_end(false),
     m_sizeX(0),
     m_sizeY(0),
-    m_textFormatSegoe(nullptr),
-    m_solidBrushWhite(nullptr)
+    m_d2dFactory(nullptr),
+    m_d2dTarget(nullptr),
+    m_directWriteFactory(nullptr),
+    m_drawThread(INVALID_HANDLE_VALUE),
+    m_fcs(fbw)
 {
-    m_fcs = fbw;
-    m_drawThread = INVALID_HANDLE_VALUE;
     D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, &m_d2dFactory);
-    DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory), reinterpret_cast<IUnknown**>(&m_directWriteFactory));
     D2D1_RENDER_TARGET_PROPERTIES prop = D2D1::RenderTargetProperties(
         D2D1_RENDER_TARGET_TYPE_DEFAULT,
         D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED),
@@ -166,14 +34,34 @@ D2DGauge::D2DGauge(FCS::FBW::Ptr const & fbw)
         D2D1_FEATURE_LEVEL_DEFAULT
         );
     m_d2dFactory->CreateDCRenderTarget(&prop, &m_d2dTarget);
+    DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory), reinterpret_cast<IUnknown**>(&m_directWriteFactory));
 }
 
 D2DGauge::~D2DGauge()
 {
     m_end = true;
-    if (m_drawThread != INVALID_HANDLE_VALUE) { WaitForSingleObject(m_drawThread, INFINITE); }
-    if (m_d2dFactory) { m_d2dFactory->Release(); }
-    if (m_directWriteFactory) { m_directWriteFactory->Release(); }
+
+    if (m_drawThread != INVALID_HANDLE_VALUE)
+    {
+        WaitForSingleObject(m_drawThread, INFINITE);
+    }
+
+    CloseHandle(m_drawThread);
+
+    if (m_directWriteFactory)
+    {
+        m_directWriteFactory->Release();
+    }
+
+    if (m_d2dTarget)
+    {
+        m_d2dTarget->Release();
+    }
+
+    if (m_d2dFactory)
+    {
+        m_d2dFactory->Release();
+    }
 }
 
 void D2DGauge::SetCanvas(ELEMENT_STATIC_IMAGE * canvas)
@@ -204,38 +92,24 @@ void D2DGauge::Callback(GAUGEHDR * pgauge, int service_id)
         if (m_canvas->hdc || m_canvas->hdc != INVALID_HANDLE_VALUE)
         {
             m_end = true;
+
             if (m_drawThread != INVALID_HANDLE_VALUE)
             {
                 WaitForSingleObject(m_drawThread, INFINITE);
             }
 
+            CloseHandle(m_drawThread);
+
             m_sizeX = m_canvas->image_data.final->dim.x;
             m_sizeY = m_canvas->image_data.final->dim.y;
 
-            RECT re;
-            re.top = 0;
-            re.left = 0;
-            re.bottom = static_cast<long>(m_sizeY);
-            re.right = static_cast<long>(m_sizeX);
+            RECT rect;
+            rect.top = 0;
+            rect.left = 0;
+            rect.bottom = static_cast<long>(m_sizeY);
+            rect.right = static_cast<long>(m_sizeX);
+            m_d2dTarget->BindDC(m_canvas->hdc, &rect);
 
-            m_rectMain = D2D1_RECT_F(D2D1::RectF(10.0f, 10.0f, static_cast<float>(m_sizeX) - 20.0f, static_cast<float>(m_sizeY) - 20.0f));
-
-            m_d2dTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), &m_solidBrushWhite);
-
-            if (m_sizeY < 1000)
-            {
-                m_directWriteFactory->CreateTextFormat(L"Segoe UI", NULL, DWRITE_FONT_WEIGHT_NORMAL, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, 12.0f, L"en-US", &m_textFormatSegoe);
-            }
-            else
-            {
-                m_directWriteFactory->CreateTextFormat(L"Segoe UI", NULL, DWRITE_FONT_WEIGHT_NORMAL, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, 16.0f, L"en-US", &m_textFormatSegoe);
-            }
-
-            m_textFormatSegoe->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_LEADING);
-            m_textFormatSegoe->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_NEAR);
-            m_textFormatSegoe->SetLineSpacing(DWRITE_LINE_SPACING_METHOD_DEFAULT, 0.0f, 0.0f);
-
-            m_d2dTarget->BindDC(m_canvas->hdc, &re);
             m_end = false;
             m_drawThread = reinterpret_cast<HANDLE>(_beginthreadex(nullptr, 0, Draw, static_cast<void*>(this), 0, nullptr));
         }
@@ -247,26 +121,61 @@ void D2DGauge::Callback(GAUGEHDR * pgauge, int service_id)
 unsigned int __stdcall D2DGauge::Draw(void *args)
 {
     auto gau = static_cast<D2DGauge*>(args);
-    
-    assert(gau);
-    while (!gau->m_end)
+
+    if (auto fcs = gau->m_fcs.lock())
     {
-        if (gau->m_draw)
+        D2D1_RECT_F rectMain = D2D1_RECT_F(D2D1::RectF(10.0f, 10.0f, static_cast<float>(gau->m_sizeX) - 20.0f, static_cast<float>(gau->m_sizeY) - 20.0f));
+        ID2D1SolidColorBrush *solidBrushWhite = nullptr;
+        IDWriteTextFormat *textFormatSegoe = nullptr;
+
+        HRESULT resultText;
+        HRESULT resultBrush;
+
+        resultBrush = gau->m_d2dTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), &solidBrushWhite);
+
+        if (gau->m_sizeY < 1000)
         {
-            if (auto p = gau->m_fcs.lock())
-            {
-                gau->m_d2dTarget->BeginDraw();
-                gau->m_d2dTarget->Clear();
-
-                auto text = Utils::s2ws(p->ToString());
-                gau->m_d2dTarget->DrawTextW(text.c_str(), text.size(), gau->m_textFormatSegoe, gau->m_rectMain, gau->m_solidBrushWhite);
-
-                gau->m_d2dTarget->EndDraw();
-                SET_OFF_SCREEN(gau->m_canvas);
-            }
-            gau->m_draw = false;
+            resultText = gau->m_directWriteFactory->CreateTextFormat(L"Segoe UI", NULL, DWRITE_FONT_WEIGHT_NORMAL, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, 12.0f, L"en-US", &textFormatSegoe);
         }
-        Sleep(20);
+        else
+        {
+            resultText = gau->m_directWriteFactory->CreateTextFormat(L"Segoe UI", NULL, DWRITE_FONT_WEIGHT_NORMAL, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, 16.0f, L"en-US", &textFormatSegoe);
+        }
+
+        if (resultText == S_OK && resultBrush == S_OK)
+        {
+            textFormatSegoe->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_LEADING);
+            textFormatSegoe->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_NEAR);
+            textFormatSegoe->SetLineSpacing(DWRITE_LINE_SPACING_METHOD_DEFAULT, 0.0f, 0.0f);
+
+            assert(gau);
+            while (!gau->m_end)
+            {
+                if (gau->m_draw)
+                {
+                    gau->m_d2dTarget->BeginDraw();
+                    gau->m_d2dTarget->Clear();
+
+                    auto text = Utils::s2ws(fcs->ToString());
+                    gau->m_d2dTarget->DrawTextW(text.c_str(), text.size(), textFormatSegoe, rectMain, solidBrushWhite);
+
+                    gau->m_d2dTarget->EndDraw();
+                    SET_OFF_SCREEN(gau->m_canvas);
+                    gau->m_draw = false;
+                }
+                Sleep(20);
+            }
+        }
+
+        if (textFormatSegoe)
+        {
+            textFormatSegoe->Release();
+        }
+
+        if (solidBrushWhite)
+        {
+            solidBrushWhite->Release();
+        }
     }
 
     return 0;
--- HornetFCS/D2D_Classes.h
+++ HornetFCS/D2D_Classes.h
@@ -17,27 +17,6 @@ namespace Gauge
 
 //-----------------------------------------------------------------------------
 
-class D2DGeo2
-{
-public:
-    D2DGeo2(ID2D1Factory*& srcfactory, bool open = true);
-    ~D2DGeo2();
-    void open_sink();
-    void close_sink();
-    void transform(ID2D1Factory*& srcfactory, const D2D1_MATRIX_3X2_F mat);
-private:
-    void transform(ID2D1Factory*& srcfactory, ID2D1TransformedGeometry*& dest_t_geo, const D2D1_MATRIX_3X2_F mat);
-    ID2D1PathGeometry* m_geo;
-    ID2D1GeometrySink* m_sink;
-    ID2D1TransformedGeometry* m_transformedGeo;
-};
-
-//-----------------------------------------------------------------------------
-
-extern ID2D1PathGeometry* combine_multiple_transformed_geometries(ID2D1Factory*& srcfactory, int geo_count, ID2D1TransformedGeometry* geos[]);
-
-//-----------------------------------------------------------------------------
-
 class D2DGauge
 {
 public:
@@ -49,20 +28,17 @@ public:
 private:
     static unsigned int __stdcall Draw(void* args);
 
-    ELEMENT_STATIC_IMAGE *m_canvas;
     ID2D1Factory *m_d2dFactory;
     ID2D1DCRenderTarget *m_d2dTarget;
     IDWriteFactory *m_directWriteFactory;
-    HANDLE m_drawThread;
 
+    HANDLE m_drawThread;
     bool m_end;
     bool m_draw;
+
+    ELEMENT_STATIC_IMAGE *m_canvas;
     int m_sizeX;
     int m_sizeY;
-
-    ID2D1SolidColorBrush *m_solidBrushWhite;
-    IDWriteTextFormat *m_textFormatSegoe;
-    D2D1_RECT_F m_rectMain;
 
     std::weak_ptr<FCS::FBW> m_fcs;
 };
